1264b3956ca73855eb3a2425cf4c3257072590e7
Martin Chamambo <martin.chamambo@datacom.co.nz>
2023-12-25 22:01:56 +1300

    Changes to be committed:

---
+++ azure-tf-complex-features/modules/azurerm-route-table/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source                = "hashicorp/azurerm"
+      version               = "~>3.0"
+      configuration_aliases = [azurerm]
+    }
+  }
+}
+
+
+resource "azurerm_route_table" "rt" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dynamic "route" {
+    for_each = var.routes
+    content {
+      name                   = route.value.name
+      address_prefix         = route.value.address_prefix
+      next_hop_type          = route.value.next_hop_type
+      next_hop_in_ip_address = lookup(route.value, "next_hop_in_ip_address", null)
+    }
+  }
+  disable_bgp_route_propagation = var.disable_bgp_route_propagation
+#   tags                          = var.tags
+}
+
+resource "azurerm_subnet_route_table_association" "spoke-to-firewall_01" {
+  for_each = toset(var.subnet_ids)
+  subnet_id      = each.value
+  route_table_id = azurerm_route_table.rt.id
+}

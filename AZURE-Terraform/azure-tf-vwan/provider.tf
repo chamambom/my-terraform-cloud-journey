c87d99af730b4a80ef33e5a387691ccb03e5570e
Martin Chamambo <chamambom@gmail.com>
2024-03-12 12:22:51 +1300

    Changes to be committed:

--- AZURE-Terraform/azure-tf-vwan/provider.tf
+++ AZURE-Terraform/azure-tf-vwan/provider.tf
@@ -3,12 +3,10 @@ terraform {
   required_providers {
     azurerm = {
       # Specify what version of the provider we are going to utilise
-      source  = "hashicorp/azurerm"
-      version = ">= 3.93.0"
+      source = "hashicorp/azurerm"
     }
     random = {
-      source  = "hashicorp/random"
-      version = ">= 3.6.0"
+      source = "hashicorp/random"
     }
   }
 }
